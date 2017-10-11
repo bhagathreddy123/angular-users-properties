e0eca4554e6366dae8a77ab8085ad8fa5420bffc
bhagath <bhagath@getcustomexposure.com>
2017-10-11 18:00:16 +0530

    creting mock data for users in angular 2 application:

--- app/users/users.component.js
+++ app/users/users.component.js
@@ -9,6 +9,26 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
 var UsersComponent = /** @class */ (function () {
     function UsersComponent() {
+        this.users = [
+            {
+                name: "virat",
+                phone_no: '8475894032',
+                email: 'virat@gmail.com',
+                updated_at: '11/11/17'
+            },
+            {
+                name: "anil",
+                phone_no: '8475814032',
+                email: 'anil@gmail.com',
+                updated_at: '11/11/17'
+            },
+            {
+                name: "uday",
+                phone_no: '8475894032',
+                email: 'uday@gmail.com',
+                updated_at: '11/11/17'
+            }
+        ];
     }
     UsersComponent = __decorate([
         core_1.Component({
--- app/users/users.component.ts
+++ app/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './user';
 
 @Component({
   moduleId: module.id,
@@ -7,8 +8,24 @@ import { Component } from '@angular/core';
 })
 
 export class UsersComponent {
-
+users: User[] = [
+  {
+  name: "virat",
+  phone_no: '8475894032',
+  email: 'virat@gmail.com',
+  updated_at: '11/11/17'
+  },
+  {
+  name: "anil",
+  phone_no: '8475814032',
+  email: 'anil@gmail.com',
+  updated_at: '11/11/17'
+  },
+  {
+  name: "uday",
+  phone_no: '8475894032',
+  email: 'uday@gmail.com',
+  updated_at: '11/11/17'
+  }
+]
 }
-
-
-import { Component } from '@angular/core';

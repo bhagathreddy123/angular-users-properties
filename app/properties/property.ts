b1c851fd5357b4a03d63501773d943b7014da5ae
bhagath <bhagath@getcustomexposure.com>
2017-10-11 18:13:19 +0530

    creating an angular 2 data Model for Property

---
+++ app/properties/property.ts
@@ -0,0 +1,11 @@
+export class Property {
+  constructor(
+    public id?: number,
+    namer?: string,
+    price?: string,
+    area?: string,
+    city?: string,
+    location? string,
+    description: string,
+  ) {}
+}
